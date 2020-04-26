2d81e2c8cd57ba9cbc9038ad06339470f531b7e9
SSkezane <63531880+SSkezane@users.noreply.github.com>
2020-04-26 17:05:51 +0900

    Update script.js
    植物の繁殖エリアと繁殖パターンを作成しました。動作確認では繁殖エリアが明確に分かれず、86行付近が上手く機能していない印象でした。

--- script.js
+++ script.js
@@ -56,14 +56,71 @@ class Point {
     }
 }
 
+
+/*
 // draw test
 var f1 = new Point(5,5);
 var f2 = new Point(24,60);
 
 var foods = [f1,f2];
+*/
+
+var plants = [];
+var plantAreas = [];
 var animals = [];
 var biome = [];
 var day = 0;
+
+
+// the frequency of updating plant
+var plantUpdateFrequency = 100;
+
+// the number of the plantAreas
+var plantAreas_number = 8;
+
+// the number of the plants
+var plant_number = 100;
+
+
+
+// create plant area
+class PlantArea {
+    constructor(minX,rangeX,minY,rangeY){
+        this.plantAreaX = Math.floor(Math.random()*rangeX + minX)/5;
+        this.plantAreaY = Math.floor(Math.random()*rangeY + minY)/5;   
+    }
+}
+
+
+
+// add to plant areas
+for(var i = 0; i < plantAreas_number; i++){
+    // set area size
+    var pA = new PlantArea(Math.floor(Math.random()*410),Math.floor(Math.random()*20+50),Math.floor(Math.random()*570),Math.floor(Math.random()*20+50));
+    
+    // add new plant area
+    plantAreas.push(pA);
+    
+}
+
+
+
+function add_plant(){
+    // add to plant in the plant area
+    for(var i = 0; i < plantAreas_number; i++){
+        var p = new Point(Math.floor(Math.random()*plantAreas[i].plantAreaX)*5, Math.floor(Math.random()*plantAreas[i].plantAreaY)*5);
+    
+        // add new plant
+        plants.push(p);
+        
+        if(plants.length > plant_number){
+            // delete old plant
+            plants.shift();
+        }
+        
+    }
+}
+
 
 function gameRoutine(){
     // fresh screen
